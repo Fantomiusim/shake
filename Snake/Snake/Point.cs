7ff3b306d8ed5ac733b8eecc75e78cb65156a23f
oleg <oplov-13@yandex.ru>
2021-04-22 16:09:52 +0300

    Snake eat

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -51,6 +51,14 @@ namespace Snake
             }
         }
 
+
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
+
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -33,24 +33,35 @@ namespace Snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
             snake.Move();
+
+            //Создание еды
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
             
-            //Движение змейки
+
+            
             while(true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
                 if(Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                    
                 }
-                Thread.Sleep(100);
-                snake.Move();
             }
-
-
-
-            
-        
         }
     }
 }
